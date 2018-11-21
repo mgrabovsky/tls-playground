6b8f72da63ae0988f96084569df0485af9f35479
Matěj Grabovský <xgrabovs@fi.muni.cz>
2018-11-21 23:59:29 +0100

    OpenSSL: Certificate verification and minimal version

--- openssl/connect.c
+++ openssl/connect.c
@@ -19,8 +19,8 @@
  *      3.  It is not used here, but OpenSSL has its own way of handling and passing
  *          error codes and messages. It is similar to standard errno, but additional
  *          care needs to be given.
- *      4.  Certificate and hostname verification, again not shown, is complex,
- *          unintuitive and unpredictable.
+ *      4.  Rudimentary certificate and hostname verification is actually easier than
+ *          expected.
  *      5.  ...
  *
  * Sources:
@@ -40,17 +40,26 @@
 #include <unistd.h>
 
 #include <openssl/bio.h>
+#include <openssl/err.h>
 #include <openssl/ssl.h>
 
-#define HOST "www.example.com"
+#define HOST "expired.badssl.com"
 #define PORT "443"
 
 #define BUFFER_SIZE 1024
 
-/* Rudimentary error handling -- just report where it occurred and bail. */
-#define FAIL() do { \
-        fprintf(stderr, "Error on line %d.\n", __LINE__); \
+/* Macros for easier error handling. */
+#define OPENSSL_FAIL() do { \
         ret = 1; \
+        ERR_print_errors_fp(stderr); \
+        goto cleanup; \
+    } while (0)
+#define OPENSSL_CHECK(x) if ((x) != 1) { \
+        OPENSSL_FAIL(); \
+    }
+#define CUSTOM_FAIL(error) do { \
+        ret = 1; \
+        fprintf(stderr, "Error: %s\n", error); \
         goto cleanup; \
     } while (0)
 
@@ -74,30 +83,27 @@ int main(void) {
 
     /* No explicit initialisation is needed as of OpenSSL 1.1.0. */
 
-    /* Create an SSL/TLS context. */
-    {
-        /* Negotiate the highest version of TLS possible. */
-        const SSL_METHOD *method = TLS_client_method();
-        if (method == NULL) {
-            FAIL();
-        }
-
-        /* Create TLS context. */
-        ctx = SSL_CTX_new(method);
-        if (ctx == NULL) {
-            FAIL();
-        }
+    /* Create TLS context. Negotiate the highest version of TLS possible. */
+    ctx = SSL_CTX_new(TLS_client_method());
+    if (ctx == NULL) {
+        OPENSSL_FAIL();
     }
+
+    /* Require TLS version at least 1.2. */
+    OPENSSL_CHECK(SSL_CTX_set_min_proto_version(ctx, TLS1_2_VERSION));
+
+    /* Require verification of the server certificate. */
+    SSL_CTX_set_verify(ctx, SSL_VERIFY_PEER, NULL);
+    SSL_CTX_set_verify_depth(ctx, 5);
+    /* Use default system-wide certificate store. */
+    SSL_CTX_set_default_verify_paths(ctx);
 
     /* Create TCP/IP socket and connect. */
     {
         BIO_ADDRINFO *result = NULL;
 
-        if (BIO_lookup_ex(HOST, PORT, BIO_LOOKUP_CLIENT, AF_UNSPEC, SOCK_STREAM,
-                    IPPROTO_TCP, &result) != 1)
-        {
-            FAIL();
-        }
+        OPENSSL_CHECK(BIO_lookup_ex(HOST, PORT, BIO_LOOKUP_CLIENT, AF_UNSPEC,
+                    SOCK_STREAM, IPPROTO_TCP, &result));
 
         const BIO_ADDRINFO *ai = result;
         while (ai != NULL) {
@@ -111,84 +117,60 @@ int main(void) {
         BIO_ADDRINFO_free(result);
 
         if (sock < 0) {
-            FAIL();
+            CUSTOM_FAIL("Could not connect to the server.");
         }
 
-        if (BIO_connect(sock, BIO_ADDRINFO_address(ai), BIO_SOCK_NODELAY) != 1) {
-            FAIL();
-        }
+        OPENSSL_CHECK(BIO_connect(sock, BIO_ADDRINFO_address(ai), BIO_SOCK_NODELAY));
     }
-
-    /* Alternative version using POSIX sockets as spotted in the OWASP example. */
-    /*
-    {
-        struct addrinfo hints = { 0 };
-        hints.ai_family   = AF_UNSPEC;
-        hints.ai_socktype = SOCK_STREAM;
-        hints.ai_flags    = AI_ADDRCONFIG | AI_NUMERICSERV;
-        hints.ai_protocol = IPPROTO_TCP;
-
-        struct addrinfo *result = NULL;
-
-        if (getaddrinfo(HOST, PORT, &hints, &result) != 0 ||
-                result == NULL)
-        {
-            FAIL();
-        }
-
-        struct addrinfo *rr = result;
-        while (rr != NULL) {
-            sock = socket(rr->ai_family, rr->ai_socktype, rr->ai_protocol);
-            if (sock >= 0) {
-                break;
-            }
-            rr = rr->ai_next;
-        }
-
-        if (sock < 0) {
-            FAIL();
-        }
-
-        if (connect(sock, rr->ai_addr, rr->ai_addrlen) != 0) {
-            FAIL();
-        }
-    }
-    */
 
     /* Create TLS channel. */
     ssl = SSL_new(ctx);
     if (ssl == NULL) {
-        FAIL();
+        OPENSSL_FAIL();
     }
 
-    if (SSL_set_tlsext_host_name(ssl, HOST) != 1) {
-        FAIL();
-    }
+    /* Set the host name for Server Name Indication. */
+    OPENSSL_CHECK(SSL_set_tlsext_host_name(ssl, HOST));
+    /* Set the host name for certificate verification. */
+    OPENSSL_CHECK(SSL_set1_host(ssl, HOST));
 
     /* Input/output channel. */
     BIO *bio = BIO_new_socket(sock, BIO_NOCLOSE);
     if (bio == NULL) {
-        FAIL();
+        OPENSSL_FAIL();
     }
 
     SSL_set_bio(ssl, bio, bio);
 
     /* Initiate TLS connection. */
-    if (SSL_connect(ssl) != 1) {
-        FAIL();
+    OPENSSL_CHECK(SSL_connect(ssl));
+
+    /* Check that the server sent a certificate. */
+    {
+        X509 *cert = SSL_get_peer_certificate(ssl);
+        if (cert == NULL) {
+            CUSTOM_FAIL("Server did not send certificate -- will not connect.");
+        }
+        X509_free(cert);
+        /* TODO: Check revocation status (CRL/OCSP). */
+    }
+
+    /* Check if the certificate was was verified successfully. */
+    if (SSL_get_verify_result(ssl) != X509_V_OK) {
+        CUSTOM_FAIL("Could not verify server certificate.");
     }
 
     const char **line = request_lines;
     while (*line) {
         if (SSL_write(ssl, *line, strlen(*line)) <= 0) {
-            FAIL();
+            OPENSSL_FAIL();
         }
         ++line;
     }
 
     char buffer[BUFFER_SIZE + 1] = { 0 };
     if (SSL_read(ssl, buffer, BUFFER_SIZE) <= 0) {
-        FAIL();
+        OPENSSL_FAIL();
     }
     printf("\x1b[34mread %zu bytes:\x1b[0m\n%s\x1b[34m###\x1b[0m\n",
             strlen(buffer), buffer);
@@ -201,7 +183,6 @@ cleanup:
     }
     if (sock >= 0) {
         BIO_closesocket(sock);
-        //close(sock);
     }
     if (ctx != NULL) {
         SSL_CTX_free(ctx);

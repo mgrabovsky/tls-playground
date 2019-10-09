c196d9c9d80d0637191461198ce920ddf9f23cb6
Martin Ukrop <mukrop@mail.muni.cz>
2019-10-09 11:16:26 +0200

    Stop using deprecated mbedTLS header file

--- mbedtls/skeleton/mbedtls.c
+++ mbedtls/skeleton/mbedtls.c
@@ -7,7 +7,7 @@
 #include <mbedtls/ctr_drbg.h>
 #include <mbedtls/entropy.h>
 #include <mbedtls/error.h>
-#include <mbedtls/net.h>
+#include <mbedtls/net_sockets.h>
 #include <mbedtls/ssl.h>
 
 #define DEFAULT_HOST "example.com"
